0fb45ae9f2e9cbd64c4f7d9ed80546589077b22d
Arthur Ribeiro <arthurribeirotj@gmail.com>
2024-05-21 14:11:49 -0300

    update project

--- app_API_Produtos/serializers.py
+++ app_API_Produtos/serializers.py
@@ -1,11 +1,7 @@
 from rest_framework import serializers
 from .models import Product
 
-# Definição do serializer para a classe Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
-        # Indica qual modelo será serializado por este serializer
         model = Product
-        # Define quais campos do modelo serão incluídos na serialização.
-        # Usar '__all__' significa que todos os campos do modelo serão incluídos
         fields = '__all__'
--- app_API_Produtos/views.py
+++ app_API_Produtos/views.py
@@ -1,9 +1,9 @@
+from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
-
 
 
 
@@ -25,7 +25,7 @@ def product_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Função de detalhes de produto para métodos GET, PUT e DELETE
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(['GET', 'PATCH', 'DELETE'])
 def product_detail(request, pk):
     try:
         # Tenta encontrar um produto com o ID fornecido na requisição
@@ -40,7 +40,7 @@ def product_detail(request, pk):
         return Response(serializer.data)
     
     # Atualiza os detalhes do produto se o método for PUT
-    elif request.method == 'PUT':
+    elif request.method == 'PATCH':
         serializer = ProductSerializer(product, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -51,67 +51,4 @@ def product_detail(request, pk):
     elif request.method == 'DELETE':
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-
-
-
-# # Decorador para indicar que esta função é uma view de API RESTful que responde aos métodos GET e POST
-# @api_view(['GET', 'POST'])
-# def product_list(request):
-#     # Verifica o método HTTP da requisição
-#     if request.method == 'GET':
-#         # Recupera todos os produtos do banco de dados
-#         products = Product.objects.all()
-#         # Serializa os produtos para transformá-los em JSON
-#         serializer = ProductSerializer(products, many=True)
-#         # Retorna uma resposta HTTP com os dados serializados
-#         return Response(serializer.data)
-    
-#     elif request.method == 'POST':
-#         # Serializa os dados recebidos na requisição em um objeto ProductSerializer
-#         serializer = ProductSerializer(data=request.data)
-#         # Verifica se os dados são válidos
-#         if serializer.is_valid():
-#             # Salva os dados no banco de dados
-#             serializer.save()
-#             # Retorna uma resposta HTTP com os dados serializados e o código de status 201 Created
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         # Caso os dados não sejam válidos, retorna uma resposta com os erros de validação e o código de status 400 Bad Request
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# # Decorador para indicar que esta função é uma view de API RESTful que responde aos métodos GET, PUT e DELETE
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def product_detail(request, pk):
-#     try:
-#         # Tenta encontrar um produto com o ID fornecido na requisição
-#         product = Product.objects.get(pk=pk)
-#     except Product.DoesNotExist:
-#         # Se o produto não for encontrado, retorna uma resposta com o código de status 404 Not Found
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-
-#     # Se o método da requisição for GET, retorna os dados do produto encontrado
-#     if request.method == 'GET':
-#         serializer = ProductSerializer(product)
-#         return Response(serializer.data)
-    
-#     # Se o método da requisição for PUT, atualiza os dados do produto com os dados fornecidos na requisição
-#     elif request.method == 'PUT':
-#         serializer = ProductSerializer(product, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-#     # Se o método da requisição for DELETE, exclui o produto do banco de dados
-#     elif request.method == 'DELETE':
-#         product.delete()
-#         # Retorna uma resposta sem conteúdo com o código de status 204 No Content
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-
-
 
